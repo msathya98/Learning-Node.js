61e4a368ed8e7404e6b44f93da496a7e09740309
Sathya M <62427113+sathya-sjce@users.noreply.github.com>
2020-12-23 19:21:25 +0530

    Creating commands in app.js.

--- node-app/app.js
+++ node-app/app.js
@@ -1,24 +1,74 @@
-/*const fs = require("fs");
-
-fs.writeFileSync('notes.txt', 'file will change see');
-fs.appendFileSync('notes.txt', '  i apppend this data', 'utf8');*/
 //require('./utils.js');
-const note = require('./notes.js');
+const notes = require('./notes.js');
 //const validator = require('validator');
 //const name = 'sathya'
 const chalk = require('chalk')
 const nodemon = require('nodemon')
+const yargs = require('yargs')
 
-//console.log(name);
-/*function add(a, b){
-	return (a + b);
-}*/
-
-console.log(note())
-
-//console.log(validator.isEmail('sathsy@hhcom'));
-//console.log(validator.isURL('https:/ddhhdu.com'));
-console.log(chalk.red.inverse.bold('Sucess'));
 
 
 //module.exports = add;
+
+/* const command = process.argv[2]
+console.log(process.argv[2]);
+
+if(command === 'add'){
+	console.log('adding note');
+} if(command === 'remove'){
+	console.log('removing note');
+} */
+
+//console.log(process.argv)
+
+//Creating a commands
+
+yargs.command({
+	command: 'add',
+
+	builder:{
+		title: {
+		    //demandOption: true,
+            type: 'string'
+
+		},
+		body: {
+			describe: 'Note decription',
+			//demandOption: true,
+            type: 'string'
+		}
+	},
+	handler: function (argv){
+		notes.addNote(argv.title, argv.body)
+	}
+})
+
+/*yargs.command({
+	command: 'remove',
+	describe: 'Remove the note',
+	handler: function(){
+		console.log("Removing the note")
+	}
+
+})
+
+yargs.command({
+	command: 'read',
+	describe: 'Read a note',
+	handler: function(){
+		console.log("reading a note")
+	}
+})
+
+yargs.command({
+	command: 'list',
+	describe: 'listing notes',
+	handler: function(){
+		console.log("Listing all notes")
+	}
+
+})
+
+
+console.log(yargs.argv)*/
+yargs.parse();
