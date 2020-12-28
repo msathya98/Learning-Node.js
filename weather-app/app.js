1c0de965f5d6b63d2532f3d139ce9f9bec2814d6
Sathya M <62427113+sathya-sjce@users.noreply.github.com>
2020-12-28 19:56:05 +0530

    app.js of Weather-App Continue..

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,27 +1,7 @@
-const geocode = (address, callback) => {
+const request = require('request');
+const forecast = require('./utils/forecast')
 
-const url1 = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1Ijoic2F0aHlhLTE5OTgiLCJhIjoiY2tqODk2b2tyMTJibDJzbnlpaWFtNGs4biJ9.b_L89uQrpXDclltkkOsGTg';
-
-request({url: url1, json: true}, (error, response) => {
-	if(error){
-		callback("unable to connect to loaction services", undefined)
-	}else if(response.body.features.length === 0){
-		callback("Unable to find loaction. Try another search", undefined)
-
-	} else{
-		callback(undefined,{
-			latitide: response.body.features[0].center[0],
-			longitude: response.body.features[0].center[1],
-			loaction: response.body.features[0].place_name
-
-		})
-
-	}
-})
-}
-
-geocode('Bengaluru', (error, data) => {
-	console.log('Error', error)
-	console.log('Data', data)
-
+forecast(-75.7088, 44.1545, (error, data) => {
+  console.log('Error', error)
+  console.log('Data', data)
 })
