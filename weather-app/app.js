a44065a1c14206ea039bdbc2b13758b4d8d16891
Sathya M <62427113+sathya-sjce@users.noreply.github.com>
2020-12-28 19:52:57 +0530

    Handling errors through Callback function.

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,25 +1,27 @@
-const url = "http://api.weatherstack.com/current?access_key=9741b4406deb88cfc642aae3ea655757&query=12.3426750,76.6232550";
+const geocode = (address, callback) => {
 
-request({ url: url, json: true}, (error, response) => {
- const temp = (response.body.current.temperature);
- const precip = (response.body.current.percip);
+const url1 = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1Ijoic2F0aHlhLTE5OTgiLCJhIjoiY2tqODk2b2tyMTJibDJzbnlpaWFtNGs4biJ9.b_L89uQrpXDclltkkOsGTg';
 
-	console.log("It is cuurently " + temp  + " degrees out.  There is a " + response.body.current.feelslike + " Chance of Rain.");
-}) 
-
-const url1 = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1Ijoic2F0aHlhLTE5OTgiLCJhIjoiY2tqODk2b2tyMTJibDJzbnlpaWFtNGs4biJ9.b_L89uQrpXDclltkkOsGTg';
-
-request({ url: url1, json: true}, (error, response) => {
-
+request({url: url1, json: true}, (error, response) => {
 	if(error){
-		console.log("Unable to connect to weather server.")
+		callback("unable to connect to loaction services", undefined)
 	}else if(response.body.features.length === 0){
-		
-		console.log("unbale to find value undifined");
-	}else{
-		console.log(response.body.features[0].center[1]);
+		callback("Unable to find loaction. Try another search", undefined)
+
+	} else{
+		callback(undefined,{
+			latitide: response.body.features[0].center[0],
+			longitude: response.body.features[0].center[1],
+			loaction: response.body.features[0].place_name
+
+		})
+
 	}
+})
+}
 
+geocode('Bengaluru', (error, data) => {
+	console.log('Error', error)
+	console.log('Data', data)
 
-
-});
+})
