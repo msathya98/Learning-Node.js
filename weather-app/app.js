318eef64f5964f71665a0bf695eea826b46b958e
Sathya M <62427113+sathya-sjce@users.noreply.github.com>
2020-12-28 19:50:37 +0530

    Making HTTP requests in app.js

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,14 +1,25 @@
-console.log("start");
+const url = "http://api.weatherstack.com/current?access_key=9741b4406deb88cfc642aae3ea655757&query=12.3426750,76.6232550";
 
-setTimeout(() => {
-	console.log("2 second wait")
+request({ url: url, json: true}, (error, response) => {
+ const temp = (response.body.current.temperature);
+ const precip = (response.body.current.percip);
 
-}, 2000)
+	console.log("It is cuurently " + temp  + " degrees out.  There is a " + response.body.current.feelslike + " Chance of Rain.");
+}) 
 
-setTimeout(() => {
-	console.log("0 second wait")
+const url1 = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1Ijoic2F0aHlhLTE5OTgiLCJhIjoiY2tqODk2b2tyMTJibDJzbnlpaWFtNGs4biJ9.b_L89uQrpXDclltkkOsGTg';
 
-}, 0)
+request({ url: url1, json: true}, (error, response) => {
+
+	if(error){
+		console.log("Unable to connect to weather server.")
+	}else if(response.body.features.length === 0){
+		
+		console.log("unbale to find value undifined");
+	}else{
+		console.log(response.body.features[0].center[1]);
+	}
 
 
-console.log("end");
+
+});
