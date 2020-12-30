2152c9ac88b54821fdb123c3e520e2d1e25ec545
Sathya M <62427113+sathya-sjce@users.noreply.github.com>
2020-12-30 19:45:07 +0530

    app.js with view-engine hbs.

--- Web-server/app.js
+++ Web-server/app.js
@@ -1,25 +1,85 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('hbs');
+const forecast = require('./utils/forecast');
+const geocode = require('./utils/geocode');
 const app = express()
 
 const Path1 = path.join(__dirname, '/public')
+const  viewsPath = path.join(__dirname, '/templates/views')
+const  partialsPath = path.join(__dirname, '/templates/partials')
 
 console.log(Path1);
+
+app.set('view engine', 'hbs');
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
 app.use(express.static(Path1)); 
 
 app.get('', (req, res) => {
-	res.send("hello guys")
+	res.render("index", {
+		title: 'Home',
+		name: 'sathya'
+	})
 })
 
+app.get('/help', (req, res) => {
+	res.render("help", {
+		title: 'Help',
+	    name: 'sathya'
+	})
+})
+
+app.get('/about', (req, res) => {
+	res.render("about", {
+		title: 'About',
+	
+	 name: 'sathya'
+	})
+})
 
 app.get('/weather', (req, res) => {
+	if(!req.query.address){
+		return res.send({
+			error: "You must provide an address"
+		})
+	}
+
+
 	res.send({
-		weatherUpdates: 'By sathya',
+		address: response.body.features[0].place_name,
 		temp: 30
+
 	})
 	}
 )
+
+app.get('/products', (req, res) => {
+	if(!req.query.search){
+		return res.send({
+			error: "You must provide a search option"
+		})
+	}
+	console.log(req.query.search)
+	res.send({
+		products: []
+	})
+})
+
+app.get('/help/*', (req, res) => {
+	res.render("404",{
+		message1: "help article not found",
+		name: 'sathya'
+	})
+})
+
+app.get('*', (req, res) => {
+	res.render("404",{
+		message1: "404 error page not found",
+		name: 'sathya'
+	})
+})
 
 
 
