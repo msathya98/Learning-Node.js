786e661bde05074a46e0f0415b4a028b35070ca5
Sathya M <62427113+sathya-sjce@users.noreply.github.com>
2021-01-15 17:44:43 +0530

    task.js with userSchema updated.

--- Task-manager/Tasks.js
+++ Task-manager/Tasks.js
@@ -1,18 +1,25 @@
-const mongoose = require("mongoose");
-const validator = require('validator')
- 
+const mongoose = require('mongoose')
 
-const Task = mongoose.model('tasks', {
+const taskSchema = new mongoose.Schema({
 	description: {
-		type: String,
-		trim: true,
-		required: true
-	},
-	completed: {
-		type: Boolean,
-		required: false,
-		default: false
-	}
+        type: String,
+        required: true,
+        trim: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    }
+},{
+    	timestamps: true
 })
+
+const Task = mongoose.model('Task', taskSchema )
+    
 
 module.exports = Task;
