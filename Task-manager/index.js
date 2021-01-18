b0e036a983bb0f9e5dcfaa3fec9a10696007a073
Sathya M <62427113+sathya-sjce@users.noreply.github.com>
2021-01-18 19:50:25 +0530

    Index.js file updated with multer npm module.
    and  fileFilter function with three arguments.

--- Task-manager/index.js
+++ Task-manager/index.js
@@ -6,17 +6,28 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-// app.use((req, res, next) => {
-//     if (req.method === 'GET') {
-//         res.send('GET requests are disabled')
-//     } else {
-//         next()
-//     }
-// })
 
-// app.use((req, res, next) => {
-//     res.status(503).send('Site is currently down. Check back soon!')
-// })
+const multer = require('multer')
+const upload = multer({
+    dest: 'images',
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(doc|docx)$/)) {
+            return cb(new Error('Please upload a Word document'))
+        }
+
+        cb(undefined, true)
+    }
+})
+app.post('/upload', upload.single('upload'), (req, res) => {
+    res.send()
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.message })
+})
+
+
 
 app.use(express.json())
 app.use(userRouter)
@@ -25,18 +36,3 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
-
-const Task = require('./models/tasks')
-const User = require('./models/users')
-
-const main = async () => {
-    // const task = await Task.findById('5c2e505a3253e18a43e612e6')
-    // await task.populate('owner').execPopulate()
-    // console.log(task.owner)
-
-    const user = await User.findById('5c2e4dcb5eac678a23725b5b')
-    await user.populate('tasks').execPopulate()
-    console.log(user.tasks)
-}
-
-main()
