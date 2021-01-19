78c9d59ec70a6a132ea35c7c480207e0e1fd1be6
Sathya M <62427113+sathya-sjce@users.noreply.github.com>
2021-01-19 19:57:07 +0530

    users.js with sharp npm module.

--- Task-manager/Users.js
+++ Task-manager/Users.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
-const Task = require('./tasks')
+const Task = require('./task')
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -67,6 +67,7 @@ userSchema.methods.toJSON = function () {
 
     delete userObject.password
     delete userObject.tokens
+    delete userObject.avatar
 
     return userObject
 }
